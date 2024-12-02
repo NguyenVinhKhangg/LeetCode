f9e557036635d2f03ac8390a6178898efe776662
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-02 21:11:03 +0700

    Add files via upload

---
+++ Ex04_RemoveElement/Ex04_RemoveElement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex04_RemoveElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ex04_RemoveElement/Ex04_RemoveElement/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04_RemoveElement
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[result] = nums[i];
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
