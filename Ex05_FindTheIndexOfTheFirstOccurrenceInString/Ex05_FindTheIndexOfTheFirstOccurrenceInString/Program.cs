b0d866e5b3fe5cee864a13c7976df431d03c11f8
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-03 21:44:18 +0700

    Add files via upload

---
+++ Ex05_FindTheIndexOfTheFirstOccurrenceInString/Ex05_FindTheIndexOfTheFirstOccurrenceInString/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Ex05_FindTheIndexOfTheFirstOccurrenceInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            String a = "leedcode";
+            String b = "ee";
+            Console.WriteLine(solution.StrStr(a,b));
+        }
+    }
+}
---
+++ Ex05_FindTheIndexOfTheFirstOccurrenceInString/Ex05_FindTheIndexOfTheFirstOccurrenceInString/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05_FindTheIndexOfTheFirstOccurrenceInString
+{
+    internal class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    for (int j = i; j < i + needle.Length; j++)
+                    {
+                        if (haystack[j] != needle[j - i])
+                        {
+                            break;
+                        }
+                        if (j == i + needle.Length - 1)
+                        {
+                            return i;
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
