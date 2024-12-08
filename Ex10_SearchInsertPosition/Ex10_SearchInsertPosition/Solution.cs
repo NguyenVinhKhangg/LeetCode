d5302ff465b9b895433df70309e385c994443fc9
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-08 21:29:35 +0700

    Add files via upload

---
+++ Ex10_SearchInsertPosition/Ex10_SearchInsertPosition/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex10_SearchInsertPosition
+{
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target)
+                {
+                    return i;
+                }
+
+            }
+            return nums.Length;
+        }
+    }
+}
