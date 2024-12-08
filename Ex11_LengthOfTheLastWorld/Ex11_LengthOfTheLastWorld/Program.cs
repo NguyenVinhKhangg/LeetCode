4f61635a1ceeaa6a9f46648583bfb36e0e7c4c89
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-08 21:29:59 +0700

    Add files via upload

---
+++ Ex11_LengthOfTheLastWorld/Ex11_LengthOfTheLastWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex11_LengthOfTheLastWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ex11_LengthOfTheLastWorld/Ex11_LengthOfTheLastWorld/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11_LengthOfTheLastWorld
+{
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            int count = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+
+                if (s[i] == ' ')
+                {
+                    break;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+}
