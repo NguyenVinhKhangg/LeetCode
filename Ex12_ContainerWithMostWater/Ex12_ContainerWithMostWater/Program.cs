6ed3d22160c7539bd5f5a6e6ee6a88c5e57eb9a5
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-09 21:17:42 +0700

    Add files via upload

---
+++ Ex12_ContainerWithMostWater/Ex12_ContainerWithMostWater/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex12_ContainerWithMostWater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ex12_ContainerWithMostWater/Ex12_ContainerWithMostWater/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex12_ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+
+            while (left < right)
+            {
+                int area = (right - left) * Math.Min(height[left], height[right]);
+                maxArea = Math.Max(maxArea, area);
+                if (height[left] < height[right])
+                {
+                    left++;
+                }
+                else if (height[left] > height[right])
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                    right--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
