03e4b0ebac77c299ddf36031655cb0ae2c852a4a
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-10 21:24:08 +0700

    Add files via upload

---
+++ Ex13_RomanToInteger/Ex13_RomanToInteger/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex13_RomanToInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ex13_RomanToInteger/Ex13_RomanToInteger/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex13_RomanToInteger
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            // Dictionary<char,int> transList = new Dictionary<char,int>();
+            // transList.Add('I',1);
+            // transList.Add('V',5);
+            // transList.Add('X',10);
+            // transList.Add('L',50);
+            // transList.Add('C',100);
+            // transList.Add('D',500);
+            // transList.Add('M',1000);        
+
+            int result = 0;
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (transList(s[i]) >= transList(s[i + 1]))
+                {
+                    result += transList(s[i]);
+                }
+                else
+                {
+                    result -= transList(s[i]);
+                }
+            }
+            return result + transList(s[s.Length - 1]);
+        }
+
+        private int transList(char c)
+        {
+            switch (c)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+            };
+            return 0;
+        }
+    }
+}
