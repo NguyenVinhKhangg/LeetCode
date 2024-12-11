d9a695cc5d74c92c4ec65f94bec1bd7629ed53a9
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-11 21:21:19 +0700

    Add files via upload

---
+++ Ex14_LongestValidParentheses/Ex14_LongestValidParentheses/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex14_LongestValidParentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Ex14_LongestValidParentheses/Ex14_LongestValidParentheses/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex14_LongestValidParentheses
+{
+    public class Solution
+    {
+        public int LongestValidParentheses(string s)
+        {
+            int left = 0, right = 0, maxLength = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    left++;
+                }
+                else
+                {
+                    right++;
+                }
+
+                if (left == right)
+                {
+                    maxLength = Math.Max(maxLength, 2 * right);
+                }
+                else if (right > left)
+                {
+                    left = right = 0;
+                }
+            }
+
+            left = right = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ')')
+                {
+                    right++;
+                }
+                else
+                {
+                    left++;
+                }
+
+                if (left == right)
+                {
+                    maxLength = Math.Max(maxLength, 2 * left);
+                }
+                else if (left > right)
+                {
+                    left = right = 0;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+
+}
