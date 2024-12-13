2cd5c0e1fd85539fd920dbb80b909195f0879677
NguyenVinhKhangg <126032433+NguyenVinhKhangg@users.noreply.github.com>
2024-12-13 19:47:14 +0700

    Add files via upload

---
+++ Ex15_MergeSortedArray/Ex15_MergeSortedArray/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex15_MergeSortedArray
+{
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                nums1[m + i] = nums2[i];
+            }
+            Array.Sort(nums1);
+        }
+
+    }
+}
